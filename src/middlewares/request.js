bf84961c0372f8f4bb846fca9908a1f1770569b7
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2019-10-14 14:38:23 +0300

    Continue development

--- src/middlewares/request.js
+++ src/middlewares/request.js
@@ -41,17 +41,17 @@ export default ({
 		generateListMetadataFormatsResponse, generateListSetsResponse,
 		generateGetRecordResponse, generateListRecordsResponse, generateListIdentifiersResponse
 	} = responseFactory({identifierPrefix, supportEmail});
-	
+
 	return async (req, res, next) => {
 		const {query: {verb}} = req;
-		
+
 		if (!req.accepts('application/xml')) {
 			res.sendStatus(HttpStatus.NOT_ACCEPTABLE);
 			return;
 		}
-		
+
 		res.type('application/xml');
-		
+
 		try {
 			await handle();
 		} catch (err) {
@@ -61,16 +61,16 @@ export default ({
 				next(err);
 			}
 		}
-		
+
 		async function handle() {
 			const handlers = getHandlers();
-			
+
 			if (verb in handlers) {
 				return callMethod(handlers[verb]);
 			}
-			
+
 			throw new ApiError({code: ERRORS.BAD_VERB});
-			
+
 			function getHandlers() {
 				return {
 					GetRecord: {
@@ -100,44 +100,44 @@ export default ({
 						allowedParams: ['verb', 'resumptionToken']
 					}
 				};
-				
+
 				function listMetadataFormats({identifier}) {
 					if (identifier) {
 						return METADATA_FORMATS.filter(({prefix}) => prefix === identifier);
 					}
-					
+
 					return METADATA_FORMATS;
 				}
 			}
-			
+
 			async function callMethod({method, useDb, allowedParams = QUERY_PARAMETERS, requiredParams = ['verb']}) {
 				const params = await getParams(useDb);
-				
+
 				if (method) {
 					const result = await wrapMethodCall();
-					
+
 					if (params.connection) {
 						await params.connection.close();
 						logger.log('debug', 'Connection closed');
 					}
-					
+
 					return sendResponse({result, params});
 				}
-				
+
 				return sendResponse({params});
-				
+
 				async function wrapMethodCall() {
 					return new Promise(async (resolve, reject) => { // eslint-disable-line no-async-promise-executor
 						req.on('close', async () => {
 							logger.log('info', 'Request cancelled');
-							
+
 							if (params.connection) {
 								await params.connection.close();
 							}
-							
+
 							resolve();
 						});
-						
+
 						try {
 							resolve(method(params));
 						} catch (err) {
@@ -145,104 +145,104 @@ export default ({
 						}
 					});
 				}
-				
+
 				async function getParams(useDb = true) {
 					const obj = {};
-					
+
 					if (useDb) {
 						logger.log('debug', 'Requesting a new connection from the pool...');
 						obj.connection = await pool.getConnection();
 						logger.log('debug', 'Connection acquired!');
 					}
-					
+
 					if (checkArguments()) {
 						if (allowedParams.includes('resumptionToken') && 'resumptionToken' in req.query) {
 							const params = parseResumptionToken({
 								secretEncryptionKey, verb,
 								token: req.query.resumptionToken
 							});
-							
+
 							return {
 								...obj,
 								...parse(params)
 							};
 						}
-						
+
 						return {...obj, ...parse({...req.query})};
 					}
-					
+
 					throw new ApiError({verb, code: ERRORS.BAD_ARGUMENT});
-					
+
 					function checkArguments() {
 						const hasOnlyAllowed = Object.keys(req.query).every(k => allowedParams.includes(k));
 						const hasAllRequired = requiredParams.every(v => [v] in req.query);
 						return hasOnlyAllowed && hasAllRequired;
 					}
-					
+
 					function parse(obj) {
 						if (allowedParams.includes('metadataPrefix')) {
 							if (METADATA_FORMATS.some(({prefix}) => prefix === obj.metadataPrefix)) {
 								if (obj.from) {
 									obj.from = parseDatestamp(obj.from);
 								}
-								
+
 								if (obj.until) {
 									obj.until = parseDatestamp(obj.until);
 								}
-								
+
 								if (obj.identifier) {
 									// Strip prefix (Slice takes offset and the length of the prefix doesn't include the separator)
 									obj.identifier = obj.identifier.slice(identifierPrefix.length + 1);
 								}
-								
+
 								if (obj.set) {
 									if (hasSet(obj.set) === false) {
 										throw new ApiError({verb, code: ERRORS.BAD_ARGUMENT});
 									}
 								}
-								
+
 								return obj;
 							}
-							
+
 							throw new ApiError({verb, code: ERRORS.CANNOT_DISSEMINATE_FORMAT});
 						}
-						
+
 						return Object.entries(obj).reduce((acc, [key, value]) => {
 							if (key === 'verb') {
 								return acc;
 							}
-							
+
 							return {...acc, [key]: value};
 						}, {});
-						
+
 						function hasSet(set) {
 							return listSets().some(({spec}) => set === spec);
 						}
-						
+
 						function parseDatestamp(stamp) {
 							const m = moment.utc(stamp, REQUEST_DATE_STAMP_FORMATS);
-							
+
 							if (m.isValid()) {
 								return m;
 							}
-							
+
 							throw new ApiError({verb, code: ERRORS.BAD_ARGUMENT});
 						}
 					}
 				}
 			}
 		}
-		
+
 		async function sendResponse({error, result, params}) {
 			const query = clone(req.query);
 			const requestURL = `${instanceUrl}${req.path}`;
-			
+
 			if (error) {
 				return res.send(await generateErrorResponse({query, requestURL, error}));
 			}
-			
+
 			return res.send(await generatePayload(verb));
-			
+
 			async function generatePayload(method) {
 				const generators = {
 					ListSets: async () => generateListSetsResponse({requestURL, query, sets: result}),
@@ -252,14 +252,14 @@ export default ({
 						if (result) {
 							return generateGetRecordResponse({requestURL, query, ...result});
 						}
-						
+
 						return generateErrorResponse({requestURL, query, error: ERRORS.ID_DOES_NOT_EXIST});
 					},
 					ListMetadataFormats: async () => {
 						if (result.length === 0) {
 							return generateErrorResponse({query, requestURL, error: ERRORS.ID_DOES_NOT_EXIST});
 						}
-						
+
 						return generateListMetadataFormatsResponse({requestURL, query, formats: result});
 					},
 					Identify: async () => {
@@ -268,28 +268,28 @@ export default ({
 						return generateIdentifyResponse({requestURL, query, descr, earliestTimestamp});
 					}
 				};
-				
+
 				return generators[method]();
-				
+
 				async function listResources(callback) {
 					const {records, cursor, previousCursor} = result;
-					
+
 					if (records.length === 0) {
 						return generateErrorResponse({query, requestURL, error: ERRORS.NO_RECORDS_MATCH});
 					}
-					
+
 					if (cursor) {
 						const {token, tokenExpirationTime} = generateResumptionToken({
-							secretEncryptionKey, resumptionTokenTimeout,
-							cursor, ...params
+							...params,
+							secretEncryptionKey, resumptionTokenTimeout, cursor
 						});
-						
+
 						return callback({
 							requestURL, query, records, token, tokenExpirationTime,
 							cursor: previousCursor === undefined ? 0 : previousCursor
-						});						
+						});
 					}
-					
+
 					return callback({requestURL, query, records});
 				}
 			}

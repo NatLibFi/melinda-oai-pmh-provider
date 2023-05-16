d8f11682307d9b2fabd18ec0acbb8d1785e470a6
Minttu Hurme <ammsalme@users.noreply.github.com>
2023-05-16 14:51:59 +0300

    Add autAux context

--- src/app/middleware/context/index.js
+++ src/app/middleware/context/index.js
@@ -18,8 +18,9 @@ import bib from './bib';
 import autNames from './aut-names';
 import autSubjects from './aut-subjects';
 import autWorks from './aut-works';
+import autAux from './aut-auxiliary';
 
 export default ({contextName, ...params}) => {
-  const map = {bib, autNames, autSubjects, autWorks};
+  const map = {bib, autNames, autSubjects, autWorks, autAux};
   return map[contextName](params);
 };

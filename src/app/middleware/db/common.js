424a82aa1ddc284f7ce879c64273884fadfafe8f
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-11-26 15:41:40 +0200

    Sql query optimization (#48)
    * Optimize SQL queries:
    ** Use strings instead of integers in queries (columns are char -type in db)
    ** Add ORDER BY to ascertain sorting
    ** Add MAX_DOC_NUMBER so that db (hopefully) uses doc-number index and sort is fast
    ** Use EXISTS instead of JOIN for headings queries
    ** Update and add tests for query.js
    
    * 2.0.3-alpha.1

--- src/app/middleware/db/common.js
+++ src/app/middleware/db/common.js
@@ -1,3 +1,6 @@
 
 
 export const DB_TIME_FORMAT = 'YYYYMMDDHHmmsss';
+// MAX_DOC_NUMBER for optimizing queries
+// let's avoid 999999999, it has some extra functionality in Aleph
+export const MAX_DOC_NUMBER = '999999998';

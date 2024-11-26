63c65d4104dddc59a38b69f6f9af9b246545e76d
Minttu Hurme <ammsalme@users.noreply.github.com>
2024-11-26 15:45:36 +0200

    Optimize SQL queries ; Dependency update (#49)
    * Sql query optimization (#48)
    
    * Optimize SQL queries:
    ** Use strings instead of integers in queries (columns are char -type in db)
    ** Add ORDER BY to ascertain sorting
    ** Add MAX_DOC_NUMBER so that db (hopefully) uses doc-number index and sort is fast
    ** Use EXISTS instead of JOIN for headings queries
    ** Update and add tests for query.js
    
    
    * Bump @natlibfi/fixugen-http-server in the development-dependencies group (#47)
    
    Bumps the development-dependencies group with 1 update: [@natlibfi/fixugen-http-server](https://github.com/natlibfi/fixugen-http-server-js).
    
    
    * Bump @natlibfi/melinda-backend-commons (#46)
    
    Bumps the production-dependencies group with 1 update: [@natlibfi/melinda-backend-commons](https://github.com/natlibfi/melinda-backend-commons-js).
    
    * Update deps
    
    * 2.0.3-alpha.1

--- src/app/middleware/db/queryOld2.js
+++ src/app/middleware/db/queryOld2.js
@@ -1,18 +1,23 @@
-import {DB_TIME_FORMAT, MAX_DOC_NUMBER} from './common';
+import {DB_TIME_FORMAT} from './common';
 
 export default ({library, limit}) => ({
   getSingleRecord: ({identifier}) => {
     if (identifier) {
       return {
         args: {identifier},
-        query: `SELECT id, time, z00_data record FROM (SELECT z13_rec_key id, z13_upd_time_stamp time FROM ${library}.z13 WHERE z13_rec_key = ':identifier') JOIN ${library}.z00 ON id = z00_doc_number`
+        query: `
+       SELECT id, time, z00_data record FROM (
+         SELECT z13_rec_key id, z13_upd_time_stamp time FROM ${library}.z13
+         WHERE z13_rec_key = :identifier
+       )
+       JOIN ${library}.z00 ON id = z00_doc_number`
       };
     }
     throw new Error(`getSingleRecord needs identifier`);
   },
   getHeadingsIndex: ({value}) => ({
     args: {value},
-    query: `SELECT z01_acc_sequence id FROM ${library}.z01 WHERE z01_rec_key like ':value'`
+    query: `SELECT z01_acc_sequence id FROM ${library}.z01 WHERE z01_rec_key like :value`
   }),
   getEarliestTimestamp: () => ({
     query: `SELECT MIN(z13_upd_time_stamp) time FROM ${library}.z13`
@@ -54,11 +59,17 @@ export default ({library, limit}) => ({
     function genQuery() {
       const conditions = generateTimeConditions();
       const indexStatements = generateIndexStatements();
-      return `SELECT id, time, z00_data record FROM (SELECT z13_rec_key id, z13_upd_time_stamp time FROM ${library}.z13 s1 WHERE s1.z13_rec_key > ':cursor' AND s1.z13_rec_key <= '${MAX_DOC_NUMBER}' ${conditions} ${indexStatements} ORDER BY s1.z13_rec_key FETCH NEXT ${limit + 1} ROWS ONLY) JOIN ${library}.z00 ON id = z00_doc_number`;
+      return `
+        SELECT id, time, z00_data record FROM (
+          SELECT z13_rec_key id, z13_upd_time_stamp time FROM ${library}.z13 s1
+          ${indexStatements}
+          WHERE s1.z13_rec_key > :cursor ${conditions}
+          FETCH NEXT ${limit + 1} ROWS ONLY)
+          JOIN ${library}.z00 ON id = z00_doc_number`;
 
       function generateTimeConditions() {
-        const start = `s1.z13_upd_time_stamp >= ':startTime'`;
-        const end = `s1.z13_upd_time_stamp <= ':endTime'`;
+        const start = 's1.z13_upd_time_stamp >= :startTime';
+        const end = 's1.z13_upd_time_stamp <= :endTime';
         if (startTime && endTime) {
           return `AND ${start} AND ${end}`;
         }
@@ -73,8 +84,7 @@ export default ({library, limit}) => ({
 
       function generateIndexStatements() {
         if (indexes.heading) {
-        // return indexes.heading.map((value, index) => `JOIN ${library}.z02 h${index} ON z13_rec_key = h${index}.z02_doc_number AND h${index}.z02_rec_key LIKE '${value}'`).join(' ');
-          return indexes.heading.map((value, index) => `AND EXISTS (SELECT 1 FROM ${library}.z02 h${index} WHERE h${index}.z02_rec_key = CONCAT('${value}', s1.z13_rec_key))`).join(' ');
+          return indexes.heading.map((value, index) => `JOIN ${library}.z02 h${index} ON z13_rec_key = h${index}.z02_doc_number AND h${index}.z02_rec_key LIKE '${value}'`).join('\n');
         }
         return '';
       }
